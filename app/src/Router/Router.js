0bb3a4d54e821d466c1f207e4682bc5f32b568ac
sumanth234 <funnyfunko@gmail.com>
2018-01-04 19:43:41 +0530

    splash

--- app/src/Router/Router.js
+++ app/src/Router/Router.js
@@ -4,22 +4,29 @@ import { Router, Scene, Actions } from 'react-native-router-flux';
 import { connect } from 'react-redux';
 import TaskList from '../Components/TaskList'
 import TaskListDetail from '../Components/TaskListDetail'
+import WelcomePage from '../Components/splash'
 
 const Scenes = Actions.create(
     <Scene key="Main">
+        <Scene
+            key='WelcomePage'
+            component={WelcomePage}
+            hideNavBar={true}
+            initial
+        />
         <Scene
             key='TaskList'
             component={TaskList}
             hideNavBar={true}
             title="Login"
-            initial
-             />
+
+        />
         <Scene
             key='TaskListDetail'
             component={TaskListDetail}
             hideNavBar={true}
             title = "Home"
-            />
+        />
     </Scene>
 )
 
